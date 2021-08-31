40e8c0d8bb92ab04ebbf7fe8cc76553f56b297d2
yaser <yassersharifi74@gmail.com>
2021-08-31 21:09:10 +0430

    install bootstrap

--- login.php
+++ login.php
@@ -1,26 +1,47 @@
-<!DOCTYPE html>
-<html lang="fa" dir="rtl">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>صفحه ورود || یاسر شریفی زاده</title>
-</head>
-<body>
-   <div>
-       <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-            <p>
-                <label for="username">نام کاربری</label>
-                <input type="text" name="username" id="username" autofocus required>
-            </p>
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            <p>
-                <label for="password">نام کاربری</label>
-                <input type="password" name="password" id="password" required>
-            </p>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
 
-            <button type="submit" name="login">ورود</button>
-        </form>
-   </div> 
-</body>
+    <title>php login oop</title>
+  </head>
+  <body>
+      <div class="login-page">
+        <div class="container">
+            <div class="row" style="width: 100%;
+height: 100vh;display: flex;
+justify-content: center;
+align-items: center">
+                <div class="col-12 col-md-6 col-lg-4">
+                   <div class="card" style="border: 0;
+box-shadow: 0 0 10px 0px #eee;
+border-radius: 10px;padding: 20px">
+                       <div class="card-body">
+                            <form action="#">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1">@</span>
+                                    <input type="text" class="form-control" placeholder="نام کاربری" aria-label="Username" aria-describedby="basic-addon1">
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon2">@</span>
+                                    <input type="text" class="form-control" placeholder="رمز عبور" aria-label="password" aria-describedby="basic-addon2">
+                                </div>
+
+                                <button type="button" class="btn btn-primary">ورود</button>
+                            </form>
+                       </div>
+                   </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
 </html>
