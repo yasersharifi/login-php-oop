09e7bfe0ec5e2a756c30eed1bc147bafa48e0eee
yaser <yassersharifi74@gmail.com>
2021-09-01 16:49:46 +0430

    add style files

--- login.php
+++ login.php
@@ -9,6 +9,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- Shabnam Fort CDN -->
+    <link href='https://cdn.fontcdn.ir/Font/Persian/Shabnam/Shabnam.css' rel='stylesheet' type='text/css'>
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="assets/style.css">
+
     <title>php login oop</title>
   </head>
   <body>
@@ -16,21 +21,41 @@
         <div class="container">
             <div class="row" style="width: 100%;height: 100vh;display: flex;justify-content: center;align-items: center">
                 <div class="col-12 col-md-6 col-lg-4">
-                   <div class="card" style="border: 0;box-shadow: 0 0 10px 0px #eee;border-radius: 10px;padding: 20px">
-                       <div class="card-body">
-                            <form action="#">
+                   <div class="card login-card">
+<!--                        <div class="card-header bg-primary">
+                           <h3 class="text-center text-white">
+                               صفحه ورود
+                           </h3>
+                       </div> -->
+                       <div class="card-body py-4 px-5">
+                            <div class="icon-area d-flex flex-row justify-content-center align-items-center pb-4">
+                                <i class="bi bi-lock text-danger" style="font-size: 50px;"></i>
+                            </div>
+                            <form action="#" class="">
                                 <div class="input-group mb-3">
-                                    <span class="input-group-text" id="basic-addon1">@</span>
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-person"></i></span>
                                     <input type="text" class="form-control" placeholder="نام کاربری" aria-label="Username" aria-describedby="basic-addon1">
                                 </div>
 
                                 <div class="input-group mb-3">
-                                    <span class="input-group-text" id="basic-addon2">@</span>
+                                    <span class="input-group-text" id="basic-addon2"><i class="bi bi-key"></i></span>
                                     <input type="text" class="form-control" placeholder="رمز عبور" aria-label="password" aria-describedby="basic-addon2">
                                 </div>
 
-                                <button type="button" class="btn btn-primary">ورود</button>
+                                <div class="input-group mb-3">
+                                    <input type="checkbox" class="form-check" id="remmemberMe">
+                                    <label for="remmemberMe" class="form-label ms-2"><small>مرا به خاطر بسپار  </small></label>
+                                </div>
+
+                                <div class="d-grid">
+                                    <button type="button" class="btn btn-primary">ورود</button>
+                                </div>
                             </form>
+
+                            <div class="my-3">
+                                <i class="bi bi-info-circle text-danger pt-2"></i>
+                                <a href="#" class="text-danger">فراموشی رمز عبور</a>
+                            </div>
                        </div>
                    </div>
                 </div>
