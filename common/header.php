9c4a950e1b6d2853361ce833b86e71868e84c343
yaser <yassersharifi74@gmail.com>
2021-09-04 15:23:14 +0430

    complete header and footer

--- common/header.php
+++ common/header.php
@@ -1,8 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?= isset($pageTitle) ? $pageTitle : ""; ?></title>
-</head>
-<body>
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- Shabnam Font CDN -->
+    <link href='https://cdn.fontcdn.ir/Font/Persian/Shabnam/Shabnam.css' rel='stylesheet' type='text/css'>
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="assets/style.css">
+
+    <title><?= isset($pageTitle) ? $pageTitle : ""; ?></title>
+  </head>
+  <body>
--- login.php
+++ login.php
@@ -49,25 +49,9 @@ if (isset($_POST["login"])) {
 
 
 ?>
-<!doctype html>
-<html lang="ar" dir="rtl">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <!-- Shabnam Font CDN -->
-    <link href='https://cdn.fontcdn.ir/Font/Persian/Shabnam/Shabnam.css' rel='stylesheet' type='text/css'>
-    <!-- Custom Css -->
-    <link rel="stylesheet" href="assets/style.css">
+<?php include_once "common/header.php"; ?>
 
-    <title>php login oop</title>
-  </head>
-  <body>
       <div class="login-page">
         <div class="container">
             <div class="row" style="width: 100%;height: 100vh;display: flex;justify-content: center;align-items: center">
@@ -120,7 +104,4 @@ if (isset($_POST["login"])) {
             </div>
         </div>
       </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-  </body>
-</html>
+<?php include_once "common/footer.php"; ?>
